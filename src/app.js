2811664bbe45f16c1357849cec8a0a3d7c58d698
unknown <boros2309@gmail.com>
2022-12-08 17:26:27 -0600

    Cambios

--- src/app.js
+++ src/app.js
@@ -1,28 +1,36 @@
 import express from 'express'
 import {pool} from './db.js'
-import { PORT } from './config.js';
+import { PORT } from './config.js'; 
+import ejs from 'ejs';
 
 const app =express();
 
-
+app.set('view engine', 'ejs');
 
 app.get('/', async(req, res)=>{
     const [rows] = await pool.query("SELECT * FROM USERS")
-    res.json(rows)
+    // res.json(rows)
+    res.render('index')
+    // res.send("Welcome to server")
+})
+app.get('/prueba', async(req, res)=>{
+    const [rows] = await pool.query("SELECT * FROM USERS")
+    // res.json(rows)
+    res.render('prueba')
     // res.send("Welcome to server")
 })
 
-app.get('/ping',async (req, res)=>{
-    const [result] = await pool.query('SELECT "Hello Word" as Result' )
-    console.log(result)
-    res.json(result[0])
-    // res.send("Welcome to server")
-})
+// app.get('/ping',async (req, res)=>{
+//     const [result] = await pool.query('SELECT "Hello Word" as Result' )
+//     console.log(result)
+//     res.json(result[0])
+//     // res.send("Welcome to server")
+// })
 
-app.get('/create', async (req, res)=>{
-  const result = await pool.query('INSERT INTO users(name) values ("Javier")')
- res.json(result) 
-})
+// app.get('/create', async (req, res)=>{
+//   const result = await pool.query('INSERT INTO users(name) values ("Javier")')
+//  res.json(result) 
+// })
 
 app.listen(PORT)
 console.log('Server on port 3000')
