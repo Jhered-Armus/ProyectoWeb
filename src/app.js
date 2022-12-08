979f5e20276a99ca4aca58afa4956f452670ff0b
unknown <boros2309@gmail.com>
2022-12-08 14:33:39 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import {pool} from './db.js'
+import { PORT } from './config.js';
+
+const app =express();
+
+
+
+app.get('/',(req, res)=>{
+    res.send("Welcome to server")
+})
+
+app.get('/ping',async (req, res)=>{
+    const [result] = await pool.query('SELECT "Hello Word" as Result' )
+    console.log(result)
+    res.json(result[0])
+    // res.send("Welcome to server")
+})
+
+app.get('/create', async (req, res)=>{
+  const result = await pool.query('INSERT INTO users(name) values ("Javier")')
+ res.json(result) 
+})
+
+app.listen(PORT)
+console.log('Server on port 3000')
