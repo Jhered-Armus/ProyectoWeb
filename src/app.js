44d0631ff524105e84b169bdc84d193e8716ce85
unknown <boros2309@gmail.com>
2022-12-08 18:35:17 -0600

    Avance - Floranza Floreria

--- src/app.js
+++ src/app.js
@@ -7,30 +7,18 @@ const app =express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static("public")); 
+
 app.get('/', async(req, res)=>{
     // const [rows] = await pool.query("SELECT * FROM USERS")
     // res.json(rows)
     res.render('index')
-    // res.send("Welcome to server")
 })
 app.get('/prueba', (req, res)=>{
-    // const [rows] = await pool.query("SELECT * FROM USERS")
-    // res.json(rows)
     res.render('prueba')
-    // res.send("Welcome to server")
+ 
 })
 
-// app.get('/ping',async (req, res)=>{
-//     const [result] = await pool.query('SELECT "Hello Word" as Result' )
-//     console.log(result)
-//     res.json(result[0])
-//     // res.send("Welcome to server")
-// })
-
-// app.get('/create', async (req, res)=>{
-//   const result = await pool.query('INSERT INTO users(name) values ("Javier")')
-//  res.json(result) 
-// })
 
 app.listen(PORT)
 console.log('Server on port', PORT)
